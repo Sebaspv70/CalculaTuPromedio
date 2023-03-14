72b44bc6e6b83b2e8dd2712e12bfe265c90b2aeb
SebastianPatino <sebastian.patino@udea.edu.co>
2023-03-13 19:26:33 -0500

    Punto 1 practica 2 Se realiza la aplicación de ModelViewModel

--- app/src/main/java/com/sebastianpatino/calculatupromedio/ui/main/MainActivity.kt
+++ app/src/main/java/com/sebastianpatino/calculatupromedio/ui/main/MainActivity.kt
@@ -2,65 +2,64 @@ package com.sebastianpatino.calculatupromedio.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.sebastianpatino.calculatupromedio.databinding.ActivityMainBinding
-import kotlin.math.roundToInt
+
 /*
 * Punto 1 practica 2
 * */
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
         val view = mainBinding.root
         setContentView(view)
 
+        val promObserver = Observer<Double> { prom ->
+            mainBinding.infoPromTextView.text = "La nota final del curso es: " + prom.toString()
+        }
+        mainViewModel.prom.observe(this, promObserver)
 
+        val errorObserver = Observer<Int> { error ->
+            if (error == 1) {
+                mainBinding.note60TextInputLayout.error = "Nota no valida"
+            }else if (error == 2){
+                mainBinding.note7TextInputLayout.error = "Nota no valida"
+            }else if (error == 3) {
+                mainBinding.note8TextInputLayout.error = "Nota no valida"
+            }else {
+                mainBinding.note25TextInputLayout.error = "Nota no valida"
+            }
+
+        }
+        mainViewModel.error.observe(this, errorObserver)
+
+
+        val no_errorObserver = Observer<Int> { noerror ->
+            if (noerror == 1) {
+                mainBinding.note60TextInputLayout.error = null
+            }else if (noerror == 2){
+                mainBinding.note7TextInputLayout.error = null
+            }else if (noerror == 3) {
+                mainBinding.note8TextInputLayout.error = null
+            }else {
+                mainBinding.note25TextInputLayout.error = null
+            }
+        }
+        mainViewModel.noerror.observe(this, no_errorObserver)
 
         mainBinding.calculateButton.setOnClickListener {
-            val numero1 = if(mainBinding.note60EditText.text.toString().toDouble() !in 0.0..5.0) {
-                mainBinding.note60TextInputLayout .error = "Nota no valida"
-                0.0
-            } else
-                mainBinding.note60EditText.text.toString().toDouble()
-            val numero2 = if(mainBinding.note7EditText.text.toString() == "") {
-                0.0
-            } else
-                mainBinding.note7EditText.text.toString().toDouble()
-            val numero3 = if(mainBinding.note8EditText.text.toString() == "") {
-                0.0
-            } else
-                mainBinding.note8EditText.text.toString().toDouble()
-            val numero4 = if(mainBinding.note25EditText.text.toString() == "") {
-                0.0
-            } else
-                mainBinding.note25EditText.text.toString().toDouble()
-            val prom = (numero1 * 0.6 + numero2 * 0.07 + numero3 * 0.08 + numero4 * 0.25)*100.0.roundToInt()/100.0
-            val info = "La nota final del curso es: $prom"
+            val numero1 = mainBinding.note60EditText.text.toString().toDoubleOrNull()
+            val numero2 = mainBinding.note7EditText.text.toString().toDoubleOrNull()
+            val numero3 = mainBinding.note8EditText.text.toString().toDoubleOrNull()
+            val numero4 = mainBinding.note25EditText.text.toString().toDoubleOrNull()
 
-            if (numero1 in 0.0..5.0) {
-                mainBinding.note60TextInputLayout.error=null
-                if (numero2 in 0.0..5.0) {
-                    mainBinding.note7TextInputLayout.error=null
-                    if (numero3 in 0.0..5.0) {
-                        mainBinding.note8TextInputLayout.error=null
-                        if (numero4 in 0.0..5.0) {
-                            mainBinding.note25TextInputLayout.error=null
-                            mainBinding.infoPromTextView.setText(info)
-                        }
-                        else
-                            mainBinding.note25TextInputLayout.error = "Nota no valida"
-                    }
-                    else
-                        mainBinding.note8TextInputLayout.error = "Nota no valida"
-                }
-                else
-                    mainBinding.note7TextInputLayout .error = "Nota no valida"
-            }
-            else
-                mainBinding.note60TextInputLayout .error = "Nota no valida"
-
+            mainViewModel.prom(numero1,numero2,numero3,numero4)
+            
         }
     }
 }
---
+++ app/src/main/java/com/sebastianpatino/calculatupromedio/ui/main/MainViewModel.kt
@@ -0,0 +1,42 @@
+package com.sebastianpatino.calculatupromedio.ui.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
+
+class MainViewModel: ViewModel() {
+    val prom: MutableLiveData<Double> by lazy{
+        MutableLiveData<Double>()
+    }
+    val error: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+    val noerror: MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
+
+    fun prom(num1: Double?, num2:Double?, num3:Double?, num4:Double?){
+
+
+        if (num1 != null && num1 in 0.0..5.0) {
+            noerror.value = 1
+            if (num2 != null && num2 in 0.0..5.0) {
+                noerror.value = 2
+                if (num3 != null && num3 in 0.0..5.0) {
+                    noerror.value = 3
+                    if (num4 != null && num4 in 0.0..5.0) {
+                        noerror.value = 4
+                        prom.value = ((num1 * 0.6 +num2 * 0.07 + num3 * 0.08 +
+                                num4 * 0.25) * 100.0.roundToInt())/ 100.0
+                    } else
+                        error.value = 4
+                } else
+                    error.value = 3
+            } else
+                error.value = 2
+        } else
+            error.value = 1
+
+    }
+
+}
